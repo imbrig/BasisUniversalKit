dbc8c4a65e97b6359733e1679a5b975e1e67935e
Brigido Rodriguez <brigido@image-metrics.com>
2020-07-20 14:21:17 -0700

    Adding all of the transcoder C++ files.

--- BasisUniversalKit.podspec
+++ BasisUniversalKit.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "BasisUniversalKit"
-  s.version      = "0.0.1"
+  s.version      = "0.0.3"
   s.summary      = "Load and use Basis Universal image format in iOS."
 
   s.description  = <<-DESC
--- BasisUniversalKit/BasisUniversalKit.h
+++ BasisUniversalKit/BasisUniversalKit.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char BasisUniversalKitVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <BasisUniversalKit/PublicHeader.h>
 #define BASISD_SUPPORT_PVRTC2
 #include "basisu.h"
+#include "basisu_transcoder.h"
